3fe0dcb689c29d550672589691d123d0f6077d58
meg-kuma <syou.canopus.6104@gmail.com>
2017-06-09 18:20:21 +0900

    first-commit

---
+++ aggregate_duca/levenshtein.py
@@ -0,0 +1,12 @@
+"""This is a test program."""
+from Levenshtein import distance
+from Levenshtein import jaro_winkler
+
+# 0.85以上を同一とする
+# 0.80だと確実だが集計値が不安
+STR1 = "恋をしようよ Let it snow"
+STR2 = "恋をしよーよ"
+
+
+print(distance(STR1, STR2))
+print(jaro_winkler(STR1, STR2))
