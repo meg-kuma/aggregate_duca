c29b5fe6d2a1ad3f930a700a6d7822061f944f3f
meg-kuma <syou.canopus.6104@gmail.com>
2017-06-21 20:17:22 +0900

    上位3番目までの曲名を分割する処理を追加

--- aggregate_duca/agg.py
+++ aggregate_duca/agg.py
@@ -1,17 +1,31 @@
+# !/usr/bin/env python
 from Levenshtein import jaro_winkler
+import sys
 
-#def split_name(array):
-#    """区切り文字で分割して曲ごとに値を返す.
-#    """
-
+def split_music(data):
+    """
+    区切り文字で分割して曲ごとに値を返す.
+    １位から３位までを別の配列に分解する.
+    """
+    list1 = []
+    list2 = []
+    list3 = []
+    with open(data, encoding='utf-8') as f:
+        for row in f:
+            columns = row.rstrip().split(',')
+            list1.append(columns[0])
+            list2.append(columns[1])
+            list3.append(columns[2])
+    return [list1, list2, list3]
 
 
 def aggregate(name):
-    """リストを作成してカウントしていく。
-        途中で表記ゆれのチェックを入れる
+    """
+    リストを作成してカウントしていく。
+    途中で表記ゆれのチェックを入れる
     """
     count = {}
-
+    # すでに曲名がある場合は加算。
     for val in name:
         if val in count.keys():
             count[val] += 1
@@ -25,13 +39,23 @@ def aggregate(name):
 def main():
     """曲名集計スクリプト"""
     # 曲名をファイルから呼び出す
+    argvs = sys.argv
+    # 区切り文字","ごとに上位3曲をリストに分割する
+    duca_list1, duca_list2, duca_list3 = split_music(argvs[1])
 
     # 区切り文字ごとに1つずつ取り出して集計する
-    duca_list = ['恋をしよーよ', '観覧車', '仇花', 'タイムマシン']
-    aggregate(duca_list)
+    #duca_list1 = ['恋をしよーよ', '観覧車', '仇花', 'タイムマシン']
+    #duca_list2 = ['恋をしよーよ', '観覧車', '仇花', 'タイムマシン']
+    #duca_list3 = ['恋をしよーよ', '観覧車', '仇花', 'タイムマシン']
+    # 1位から３位までの曲をそれぞれ集計する
+    count_duca1 = aggregate(duca_list1)
+    count_duca2 = aggregate(duca_list2)
+    count_duca3 = aggregate(duca_list3)
     # 既存のDuca曲のリストと集計結果を紐付けて0.85より高いものを合計していく
+
     # その際に「観覧車」「恋をしようよ」のように厄介な曲は別途合計する
 
+    # それぞれをファイルに出力する
 
 if __name__ == '__main__':
     main()
--- aggregate_duca/tests/test_agg.py
+++ aggregate_duca/tests/test_agg.py
@@ -1,6 +1,6 @@
 import unittest
 
-from agg import aggregate
+from agg import aggregate, split_music
 
 class TestAggregate(unittest.TestCase):
     def test_aggregate(self):
@@ -9,3 +9,20 @@ class TestAggregate(unittest.TestCase):
         actual = aggregate(kyoku_list) 
         self.assertEqual(actual, {'恋をしよーよ': 1, 'タイムマシン': 1, '観覧車': 1, '仇花': 1})
         #self.assertEqual(actual, {'恋をしよーよ': 1})
+
+    def test_aggregate2(self):
+        kyoku_list = ['恋をしよーよ', '観覧車', 'タイムマシン', '仇花', 'タイムマシン']
+        actual = aggregate(kyoku_list) 
+        self.assertEqual(actual, {'恋をしよーよ': 1, 'タイムマシン': 2, '観覧車': 1, '仇花': 1})
+
+    def test_aggregate_0(self):
+        kyoku_list = []
+        actual = aggregate(kyoku_list) 
+        self.assertEqual(actual, {})
+
+class TestSplitMusic(unittest.TestCase):
+    def test_split_music(self):
+        actual1, actual2, actual3 = split_music('test_data.txt')
+        self.assertEqual(actual1, ['アマオト', 'アマオト', 'ラムネ'])
+        self.assertEqual(actual2, ['Platonic syndrome', 'ロケット☆ライド', 'ロケット☆ライド'])
+        self.assertEqual(actual3, ['二人色', '観覧車', "Welcom☆Berry's"])
